47382f973cafb51a57bede451bdea8d757bfd428
MarcioGomes78 <mjgomes1978@gmail.com>
2022-08-14 15:09:58 -0300

    Criando projeto e classe controller

---
+++ src/main/java/com/mjgomes/springBootApp/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.mjgomes.springBootApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("msgBemVindo", "Bem_Vindo à biblioteca");
+        return "publica-index";
+    }
+}
