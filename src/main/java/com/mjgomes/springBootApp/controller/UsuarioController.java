eb41f8f716335610312bffb8119f620017e1659e
MarcioGomes78 <mjgomes1978@gmail.com>
2022-08-16 19:01:26 -0300

    Apagando usuário do banco de dados.

--- src/main/java/com/mjgomes/springBootApp/controller/UsuarioController.java
+++ src/main/java/com/mjgomes/springBootApp/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -16,10 +17,8 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
-
     @Autowired
     private UsuarioRepository usuarioRepository;
-
     @GetMapping("/novo")
     public String adicionarUsuario(Model model) {
         model.addAttribute("usuario", new Usuario());
@@ -38,5 +37,12 @@ public class UsuarioController {
     public String listarUsuario(Model model) {
         model.addAttribute("usuarios", usuarioRepository.findAll());
         return "/auth/admin/admin-listar-usuario";
+    }
+    @GetMapping("/admin/apagar/{id}")
+    public String deleteUser(@PathVariable("id") long id, Model model) {
+        Usuario usuario = usuarioRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("Id inválido: " + id));
+        usuarioRepository.delete(usuario);
+        return "redirect:/usuario/admin/listar";
     }
 }
